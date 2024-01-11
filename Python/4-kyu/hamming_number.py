e8af4033eb687c88d1399f294dc5e44c02b32f9f
sreenidhi nallajerla <95966587+sreenidhi-n@users.noreply.github.com>
2024-01-11 14:49:33 +0530

    Create hamming_number.py

---
+++ Python/4-kyu/hamming_number.py
@@ -0,0 +1,29 @@
+'''
+A Hamming number is a positive integer of the form 2^i*3^j*5^k, for some non-negative integers i, j, and k.
+
+Write a function that computes the nth smallest Hamming number.
+
+Specifically:
+The first smallest Hamming number is 1 = 203050
+The second smallest Hamming number is 2 = 213050
+The third smallest Hamming number is 3 = 203150
+The fourth smallest Hamming number is 4 = 223050
+The fifth smallest Hamming number is 5 = 203051
+'''
+def hamming(n):
+    res = [1]
+    i = 0
+    j = 0
+    k = 0
+    count = 1
+    while n>count:
+        num = min(res[i]*2, res[j]*3, res[k]*5)
+        if num == res[i]*2:
+            i+=1
+        if num == res[j]*3:
+            j+=1
+        if num == res[k]*5:
+            k+=1
+        count += 1
+        res.append(num)
+    return res[-1]
